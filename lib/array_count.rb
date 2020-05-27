e68de2acf6702e10a81c6d943132727c3514f85f
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-27 17:11:36 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,12 @@
+array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
+array_2 = ["", 4, "", "goodbye",""]
+
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  array.count{|e| e.is_a?(String)}
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count{|e| e == ""}
+
 end
